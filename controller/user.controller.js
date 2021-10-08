10d86b43751b64bcf6c45f5869dfed5e7e34c541
DatskoRoman <pbk.datsko@gmail.com>
2021-10-08 12:35:07 +0300

    lesson 3 practice test commit

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,28 +1,25 @@
-const db = require('../dataBase/users')
+const db = require('../dataBase/users');
 
 module.exports = {
-    getUsers: (req, res) => {
-        res.json(db);
-    },
+  getUsers: (req, res) => {
+    res.json(db);
+  },
 
-    getUserById: (req, res) => {
-        // console.log(')))))))))))');
-        // console.log(req.params);
-        // console.log(')))))))))))');
-        const { user_id} = req.params;
-        const user = db[user_id -1];
+  getUserById: (req, res) => {
+    const { user_id} = req.params;
+    const user = db[user_id -1];
 
-        res.json(user);
-    },
+    res.json(user);
+  },
 
-    createUser:(req, res) => {
-        console.log(req.body);
+  createUser:(req, res) => {
+    console.log(req.body);
 
-        db.push({...req.body, id: db.length + 1})
-        res.json(db);
-    },
+    db.push({...req.body, id: db.length + 1});
+    res.json(db);
+  },
 
-    updateUser:(req, res) => {
-        res.json('Update users');
-    }
-}
+  updateUser:(req, res) => {
+    res.json('Update users');
+  }
+};
--- routes/user.router.js
+++ routes/user.router.js
@@ -2,16 +2,12 @@ const router = require('express').Router();
 
 const userController = require('../controller/user.controller');
 
-router.get('/', userController.getUsers);
+router.get('/', userController.getUsers)
 
-router.get('/:user_id', userController.getUserById);
+router.get('/:user_id', userController.getUserById)
 
-// router.get('/:user_id/:comment_id', userController.getUserById);
+router.post('/', userController.createUser )
 
-// router.get('/:user_id/:post.id', userController.getUsers);
-
-router.post('/', userController.createUser );
-
-router.put('/', userController.updateUser);
+router.put('/', userController.updateUser)
 
 module.exports = router;
